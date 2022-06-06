9387ccf4f58b7bcd1a4f6ed356f06d21b2aa329b
Sunil <snilavarath@gmail.com>
2022-06-06 10:19:12 -0400

    blog form started

---
+++ client/src/components/BlogForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+function BlogForm({ blog, currentUser, onAddBlog }) {
+  const [title, setTitle] = useState("");
+  const [topic, setTopic] = useState("");
+  const [content, setContent] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const blogData = {
+      content: content,
+      blog_id: blog.id,
+      user_id: currentUser.id,
+      views: 0,
+    };
+    fetch("http://localhost:3000/blogs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(blogData),
+    })
+      .then((r) => r.json())
+      .then((blogData) => onAddBlog(blogData));
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <label>
+            Topic:
+            <input
+              type="text"
+              name="topic"
+              value={topic}
+              onChange={(e) => setTopic(e.target.value)}
+            />
+          </label>
+          <label>
+            Content:
+            <input
+              type="text"
+              name="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </label>
+          <button type="submit">Make Post</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default BlogForm;
